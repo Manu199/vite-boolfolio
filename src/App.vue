ee8d3e1cb86158f23adef0a462613821b5c78187
Manu199 <manu199gh@gmail.com>
2023-12-10 00:00:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      titolo: 'Bentornato in Vue'
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>{{ titolo }}</h1>
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
